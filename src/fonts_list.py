ff2b406e4ddde40f2695aec506f0ae67177f97cc
gregorni <gregorniehl@web.de>
2023-07-23 15:26:52 +0200

    fonts_list.py: Remove/replace some fonts
    and add header comment

--- src/fonts_list.py
+++ src/fonts_list.py
@@ -1,29 +1,41 @@
+# fonts_list.py
+#
+# Copyright 2023 Calligraphy Contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 FONTS_LIST = [
     "3-d",
     "5lineoblique",
     "acrobatic",
     "alligator",
     "alligator2",
-    "arrows",
     "avatar",
     "banner3-D",
-    "basic",
     "bell",
     "big",
-    "bigchief",
     "block",
-    "catwalk",
     "chunky",
     "coinstak",
-    "contessa",
     "cosmic",
     "crawford",
     "cyberlarge",
     "defleppard",
-    "diamond",
     "dotmatrix",
     "epic",
-    "fourtops",
     "fraktur",
     "fuzzy",
     "goofy",
@@ -32,17 +44,14 @@ FONTS_LIST = [
     "hollywood",
     "invita",
     "isometric1",
+    "isometric3",
     "jazmine",
     "kban",
-    "larry3d",
-    "lean",
     "letters",
     "marquee",
     "mini",
     "nancyj",
-    "nancyj-underlined",
     "nvscript",
-    "o8",
     "poison",
     "puffy",
     "relief",
@@ -50,16 +59,11 @@ FONTS_LIST = [
     "script",
     "serifcap",
     "shimrod",
-    "slant",
     "small",
     "speed",
-    "stampatello",
     "starwars",
     "thin",
-    "tiles",
-    "tinker-toy",
     "tombstone",
     "univers",
     "usaflag",
-    "weird",
 ]
